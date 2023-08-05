f272abd3e3f276b41b7d29ffcb275bed8fe4f66c
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-04 20:08:14 -0400

    Comments class completed

--- src/main/java/com/techelevator/model/Comments.java
+++ src/main/java/com/techelevator/model/Comments.java
@@ -1,4 +1,89 @@
 package com.techelevator.model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Comments {
+
+    private int id;
+    private String content;
+    private int createdBy;
+    private LocalDateTime createdAt;
+    private int ticketId;
+
+    public Comments(int id, String content, int createdBy, LocalDateTime createdAt, int ticketId) {
+        this.id = id;
+        this.content = content;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.ticketId = ticketId;
+    }
+
+    public Comments() {};
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Comments{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", createdBy=" + createdBy +
+                ", createdAt=" + createdAt +
+                ", ticketId=" + ticketId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comments comments = (Comments) o;
+        return id == comments.id && createdBy == comments.createdBy && ticketId == comments.ticketId && Objects.equals(content, comments.content) && Objects.equals(createdAt, comments.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, createdBy, createdAt, ticketId);
+    }
 }
