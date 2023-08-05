f7ab31826ff6d2bbc557d6020fbd3ba7dd248034
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-05 08:52:47 -0400

    Working buglistdao

---
+++ src/main/java/com/techelevator/dao/BugListDao.java
@@ -0,0 +1,30 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.BugList;
+
+import java.util.List;
+
+public interface BugListDao {
+
+    List<BugList> findAll();
+
+    BugList findById(int bugListId);
+
+    BugList filterByName(String name);
+
+    BugList findByUserId(int createdBy);
+
+    BugList findByListAndUserId(int bugListId, int createdBy);
+
+    void addUserToList(int bugListId, int createdBy);
+
+    BugList create(BugList newBugList);
+
+
+    BugList update(BugList modifiedBugList);
+
+    void delete(int bugListId);
+
+
+
+}
