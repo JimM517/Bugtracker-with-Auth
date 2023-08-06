7343b9dcbf865ea133e63de55d76d98fbdf8a285
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-06 17:28:44 -0400

    Fixed comment

--- src/main/java/com/techelevator/service/BugService.java
+++ src/main/java/com/techelevator/service/BugService.java
@@ -1,4 +1,16 @@
 package com.techelevator.service;
 
 public class BugService {
+
+
+    //goals of service class
+    //display each buglist
+    //get tickets by buglist id
+    //display users on buglist
+    //only authenticated users can modify bug lists and tickets/ are able to comment
+
+
+
+
+
 }
