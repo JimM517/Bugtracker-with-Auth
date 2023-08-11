2a479142553d2943f361835d0434734d559f582b
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-11 19:16:52 -0400

    Fixed comment

--- src/main/java/com/techelevator/service/BugService.java
+++ src/main/java/com/techelevator/service/BugService.java
@@ -107,6 +107,8 @@ public class BugService {
 
     }
 
+
+    //TODO finish this function!!
     public void createNewTicket(Tickets ticket, Principal principal) {
         // get our bug list
     }
